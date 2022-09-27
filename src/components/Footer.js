b7496baeb2b71775e6180a891b273ac6dabe366b
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-09-27 17:15:42 -0400

    styling changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import gitHub from "../components/assets/githublogo.png"
 import linkedIn from "../components/assets/linkedInlogo.png"
-
+import stackOverflow from "../components/assets/soLogo.png"
 
 function Footer() {
     return (
@@ -9,7 +9,7 @@ function Footer() {
             <ul className="footer" id="footer">
                 <li><a className="gitHub" href="https://github.com/gabeab34" target="_blank"><img id ="githubFooter" src = { gitHub }/></a></li>
                 <li><a className="linkedIn" href="https://www.linkedin.com/in/gabeab34/" target="_blank" ><img id ="linkedInFooter" src = { linkedIn }/></a></li>
-                
+                <li><a className="stackOverflow" href="https://stackoverflow.com/users/18819381/gabeab34" target="_blank" ><img id ="soFooter" src = { stackOverflow }/></a></li>
             </ul>
         </footer>
     )
